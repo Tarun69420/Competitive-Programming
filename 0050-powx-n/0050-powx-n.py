3920f3c730a4e8aceb67027b61bc3bbc809b78af
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-02 11:34:43 +0530

    Time: 0 ms (100%), Space: 16.7 MB (9.23%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n <0:
+            sol = poww(x,-1*n)
+            return 1/sol
+        else:
+            return poww(x,n)
+def poww(x,n):
+    if n == 0:
+        return 1
+    if n%2 == 0:
+        return poww(x*x,n//2)
+    if n%2 == 1:
+        return x*poww(x,n-1)
+
+
+
+

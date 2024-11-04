31f15fb971a2b2255bf70b37f1d00ab99a5ea1ec
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-04 22:43:54 +0530

    Time: 3 ms (95.34%), Space: 16.6 MB (57.23%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
+        self.bs(candidates,0,target,[],ans)
+        return ans
+    def bs (self,candidates,i,target,ds,ans):
+        if target == 0:
+            ans.append(ds[:])
+            return
+        if target<0:
+            return
+        for j in range(i,len(candidates)):
+            if j>i and candidates[j]==candidates[j-1]:
+                continue
+            if candidates[j] > target:
+                break
+            ds.append(candidates[j])
+            self.bs(candidates,j+1,target-candidates[j],ds,ans)
+            ds.pop()

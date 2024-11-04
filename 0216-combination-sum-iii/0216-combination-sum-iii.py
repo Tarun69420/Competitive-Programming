0fd8d829bbd405f7409c5131dfe30e1ccfa4639e
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-04 23:29:09 +0530

    Time: 0 ms (100%), Space: 16.7 MB (23.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,20 +1,16 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ans = []
-        arr=[1,2,3,4,5,6,7,8,9]
-        self.bs(arr,n,k,1,ans,[])
+        self.bs(n,k,1,ans,[])
         return ans
-    def bs(self,arr,target,n,i,ans,ds):
-        if n<0:
-            return
-        if len(ds) == n and target == 0:
+    def bs(self,target,n,i,ans,ds):
+        if n == 0 and target == 0:
             ans.append(ds[:])
             return
         for j in range(i,10):
             if j > target:
                 break
-            if target>=0:
-                ds.append(j)
-                self.bs(arr,target-j,n,j+1,ans,ds)
-                ds.pop()
+            ds.append(j)
+            self.bs(target-j,n-1,j+1,ans,ds)
+            ds.pop()
         return 

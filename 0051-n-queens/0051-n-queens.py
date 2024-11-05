a6def6d7b58f914e52b69cfca722f7adff716795
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-05 15:44:41 +0530

    Time: 43 ms (21%), Space: 16.9 MB (87.43%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,45 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = [["."]*n for i in range(n)]
+        self.solve(0,n,board,ans)
+        return ans
+    def solve(self,col,n,board,ans):
+        if col == n:
+            x = []
+            for i in board:
+                x.append("".join(i))
+            ans.append(x[:])
+            print(board)
+            return
+        for i in range(0,n):
+            if self.safe(i,col,n,board):
+                board[i][col] = "Q"
+                self.solve(col+1,n,board,ans)
+                board[i][col] = "."
+        return
+    def safe(self,row,col,n,board):
+        r = row
+        c = col
+        while r >=0 and c >=0:
+            if board[r][c] == "Q":
+                return False
+            r-=1
+            c-=1
+        r = row
+        c = col
+        while c>=0:
+            if board[r][c] =="Q":
+                return False
+            c-=1
+        r = row
+        c = col
+
+        while r<n and c>=0:
+            if board[r][c] == "Q":
+                return False
+            c-=1
+            r+=1
+        return True
+
+        

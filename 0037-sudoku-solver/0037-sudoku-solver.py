9e43e14d457bcd01152e102ff2dea1add59efddc
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-06 11:10:20 +0530

    Time: 253 ms (46.42%), Space: 16.6 MB (85.87%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,35 +1,45 @@
+from typing import List
+
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
-        Do not return anything, modify board in-place instead.
+        Solves the Sudoku puzzle by modifying `board` in-place.
         """
-        self.bs (board,0)
-    def bs (self,board,row):
+        self.bs(board, 0, 0)
+        
+    def bs(self, board: List[List[str]], row: int, col: int) -> bool:
+        # If we have completed all rows, the board is solved
         if row == 9:
             return True
-        for col in range(0,9):
-            if board[row][col] == ".":
-                for num in range(1,10):
-                    if self.valid(board,row,col,str(num)):
-                        board[row][col] = str(num)
-                        if (self.bs(board,row)) == True:
-                            return True
-                        else:
-                            board[row][col] = "."
-                return False
-        return self.bs(board,row+1)
+        # If we have reached the end of the row, move to the next row
+        if col == 9:
+            return self.bs(board, row + 1, 0)
+        # If cell is not empty, move to the next cell in the row
+        if board[row][col] != ".":
+            return self.bs(board, row, col + 1)
         
-        
-    def valid (self,board,row,col,num):
+        # Try placing numbers 1-9 in the empty cell
+        for num in range(1, 10):
+            if self.valid(board, row, col, str(num)):
+                board[row][col] = str(num)
+                if self.bs(board, row, col + 1):  # Recur with the next column
+                    return True
+                board[row][col] = "."  # Backtrack if placing num didn't work
+        return False  # No valid number could be placed in this cell
+
+    def valid(self, board: List[List[str]], row: int, col: int, num: str) -> bool:
+        # Check the row
         for i in range(9):
-            if num == board[i][col]:
+            if board[row][i] == num:
                 return False
+        # Check the column
         for i in range(9):
-            if num == board[row][i]:
+            if board[i][col] == num:
                 return False
-        x,y = row - row % 3 , col - col % 3
-        for i in range(x,x+3):
-            for j in range(y,y+3):
+        # Check the 3x3 sub-grid
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
                 if board[i][j] == num:
                     return False
         return True

578a3f9339788fad0365510b81a8638bdc851ca1
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-08 12:00:47 +0530

    Time: 0 ms (100%), Space: 17 MB (11.06%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,18 +1,20 @@
 class Solution:
-    
     def getPermutation(self, n: int, k: int) -> str:
-        d = [str(i) for i in range(1,n+1)]
-        return self.bs(n,k-1,d,"")
-        
-    def bs (self,n,k,d,ds):
-        if len(d) == 0:
-            return ds
-        nn  = self.fact(n-1)
-        ds+=d.pop(k//nn)
-        return self.bs(n-1,k%nn,d,ds)
-         
+        d = [str(i) for i in range(1, n + 1)]  # Initialize with all numbers in string format
+        fact = 1
+        for i in range(1, n):
+            fact *= i
 
-    def fact(self,a):
-        if a<=0:
-            return 1
-        return a*self.fact(a-1)
+        return self.bs(n, k - 1, d, "", fact)
+    
+    def bs(self, n, k, d, ds, fact):
+        if not d:
+            return ds
+        
+        index = k // fact
+        ds += d.pop(index)
+        k %= fact
+        if n > 1:
+            fact //= (n - 1)  # Reduce factorial for the next recursive call
+            
+        return self.bs(n - 1, k, d, ds, fact)

c2449cbc5aae94f7a5105f96bae5972f6e5a2edf
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-08 11:45:40 +0530

    Time: 3 ms (33.64%), Space: 16.7 MB (24.47%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def getPermutation(self, n: int, k: int) -> str:
+        d = [str(i) for i in range(1,n+1)]
+        return self.bs(n,k-1,d,"")
+        
+    def bs (self,n,k,d,ds):
+        if len(d) == 0:
+            return ds
+        nn  = self.fact(n-1)
+        x = k//nn
+        ds+=d[x]
+        d.pop(x)
+        return self.bs(n-1,k%nn,d,ds)
+         
+
+    def fact(self,a):
+        if a<=0:
+            return 1
+        return a*self.fact(a-1)

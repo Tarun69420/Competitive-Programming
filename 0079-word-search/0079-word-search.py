4ed462bb63b4a3f23f810702474305bc93c5f821
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-09 15:26:28 +0530

    Time: 5069 ms (26.25%), Space: 16.8 MB (20.68%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,26 @@
-from typing import List
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        
-        # Start search from every cell in the grid
-        for i in range(rows):
-            for j in range(cols):
-                if self.bs(board, word, i, j, set()):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.bs(board,word,i,j,set()):
                     return True
         return False
-    
-    def bs(self, board, word, i, j, visit):
-        # Base case: if word is empty, all characters have been matched
+    def bs (self,board,word,i,j,visit):
         if word == "":
             return True
-        
-        # Out of bounds, cell visited, or character mismatch
-        if (i, j) in visit or i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0]:
+        if (i,j) in visit or i>=len(board) or j>=len(board[0]) or i<0 or j<0 or board[i][j]!=word[0]:
             return False
 
-        # Mark the current cell as visited
-        visit.add((i, j))
-        
-        # Recursively check all four possible directions
-        if (self.bs(board, word[1:], i, j + 1, visit) or  # Right
-            self.bs(board, word[1:], i + 1, j, visit) or  # Down
-            self.bs(board, word[1:], i - 1, j, visit) or  # Up
-            self.bs(board, word[1:], i, j - 1, visit)):   # Left
-            return True
-
-        # Backtrack: unmark the cell as visited
-        visit.remove((i, j))
+        visit.add((i,j))
+        if self.bs(board,word[1:],i,j+1,visit) == True:
+                return True
+        if self.bs (board,word[1:],i+1,j,visit) == True:
+                return True
+        if self.bs (board,word[1:],i-1,j,visit) == True:
+                return True
+        if self.bs (board,word[1:],i,j-1,visit) == True:
+                return True
+        visit.discard((i,j))
         
         return False
+            

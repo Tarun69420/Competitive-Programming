f134fef1f52ff31bc0532542df6aafa798b3a996
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-09 11:21:44 +0530

    Time: 32 ms (79.05%), Space: 17.4 MB (6.42%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -3,7 +3,7 @@ class Solution:
         l = 0
         r = 0
         z = 0
-        mlen = 0
+    
         while r < len(nums):
             if nums[r] == 0:
                 z+=1   
@@ -11,7 +11,5 @@ class Solution:
                 if nums[l] == 0:
                     z-=1
                 l+=1
-            if z<=k:
-                mlen = max(r - l + 1 , mlen)
             r+=1
         return len(nums)-l

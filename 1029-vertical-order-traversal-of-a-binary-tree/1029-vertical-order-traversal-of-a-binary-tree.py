40236c540dbd37e1ef42d709ed52f780a2d06bef
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-26 16:27:32 +0530

    Time: 0 ms (100%), Space: 17.5 MB (15.08%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
@@ -1,31 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import defaultdict
+from heapq import heappush, heappop
+
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        q = [[root,0,0]]
-        d = {}
+        # Use a defaultdict to store nodes at each vertical and horizontal level.
+        nodes = defaultdict(list)
+        q = [(0, 0, root)]  # (horizontal distance, vertical level, node)
+
         while q:
-            n = len(q)
-            
-            for i in range(n):
-                r,x,y = q.pop(0)
-                if r.left:
-                    q.append([r.left,x+1,y-1])
-                if r.right:
-                    q.append([r.right,x+1,y+1])
-                if y not in d:
-                    d[y]=[]
-                d[y].append([x,r.val])
+            hd, level, node = q.pop(0)
+
+            # Store the node in the dictionary. We use a heap to sort by level and value.
+            heappush(nodes[hd], (level, node.val))
+
+            if node.left:
+                q.append((hd - 1, level + 1, node.left))
+            if node.right:
+                q.append((hd + 1, level + 1, node.right))
+
+        # Extract the results from the dictionary
         ans = []
-        print(d)
-        for i in sorted(d.keys()):
-            x=[]
-            for j in sorted(d[i]):
-                x.append(j[1])
-            ans.append(x)
+        for hd in sorted(nodes.keys()):  # Process horizontal distances in sorted order
+            col = []
+            while nodes[hd]:
+                _, val = heappop(nodes[hd])  # Extract nodes sorted by level and value
+                col.append(val)
+            ans.append(col)
 
         return ans

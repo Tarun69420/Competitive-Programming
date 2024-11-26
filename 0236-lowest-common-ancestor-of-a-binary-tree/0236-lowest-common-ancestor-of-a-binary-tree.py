7c7b395382c9b86f97e7193f27a24a9090a9a235
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2024-11-26 20:09:35 +0530

    Time: 52 ms (44.02%), Space: 21.6 MB (6.7%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', r1: 'TreeNode', r2: 'TreeNode') -> 'TreeNode':
+       
+        def dfs(root,r1,r2):
+            if root == r1 or root == r2 or root == None:
+                return root
+            
+            l =dfs(root.left,r1,r2)
+            r=dfs(root.right,r1,r2)
+            if l and r:
+                return root
+            return l if l else r
+        
+        x =  dfs(root,r1,r2)
+        return x if x else root

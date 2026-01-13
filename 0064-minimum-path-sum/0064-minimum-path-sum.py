8f3ccb47c127843bfe2e194e37bc5a113ca0423e
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2026-01-13 16:20:43 +0530

    Time: 25 ms (26.5%), Space: 22.1 MB (14.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,7 +2,8 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m,n = len(grid),len(grid[0])
         mem = [[-1]*n for i in range(m)]
-        return self.calpath(grid,m-1,n-1,mem)
+        #return self.calpath(grid,m-1,n-1,mem)
+        return self.tab_CP(grid)
     def calpath(self,grid,row,col,mem):
         if row == 0 and col == 0 :
             return grid[row][col]
@@ -15,3 +16,23 @@ class Solution:
 
         mem[row][col]=min(left,right)
         return mem[row][col]
+    def tab_CP(self,grid):
+        m,n = len(grid),len(grid[0])
+        mem = [[-1]*n for i in range(m)]
+
+        
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    mem[0][0] = grid[0][0]
+                else:
+                    left,right = float("inf"),float("inf")
+                    if i>0:
+                        left = grid[i][j] + mem[i-1][j]
+                    if j>0:
+                        right = grid[i][j] + mem[i][j-1]
+                    mem[i][j] = min(left,right)
+        return mem[m-1][n-1]
+            
+

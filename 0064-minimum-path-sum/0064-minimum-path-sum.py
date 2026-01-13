94a9dd446b34e51fe4ed9b85f61cbd9f60af3d5a
Tarun69420 <101054423+Tarun69420@users.noreply.github.com>
2026-01-13 16:10:46 +0530

    Time: 27 ms (25.75%), Space: 21.9 MB (15.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,18 +1,17 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        return dp(grid,len(grid)-1,len(grid[0])-1,{})
-def dp(grid,i,j,temp):
- 
- 
-        if i<0 or j<0:
-            return float('inf')
-        
-        if i == 0 and j == 0:
-            return grid[0][0]
-        if (i,j) in temp:
-            return temp[(i,j)]
-        
-        s = dp(grid,i-1,j,temp)
-        w = dp(grid,i,j-1,temp)
-        temp[(i,j)]= min(s,w)+grid[i][j]
-        return temp[(i,j)]
+        m,n = len(grid),len(grid[0])
+        mem = [[-1]*n for i in range(m)]
+        return self.calpath(grid,m-1,n-1,mem)
+    def calpath(self,grid,row,col,mem):
+        if row == 0 and col == 0 :
+            return grid[row][col]
+        if row <0 or col <0:
+            return float("inf")
+        if mem[row][col] != -1:
+            return mem[row][col]
+        left = grid[row][col]+self.calpath(grid,row-1,col,mem)
+        right = grid[row][col]+self.calpath(grid,row,col-1,mem)
+
+        mem[row][col]=min(left,right)
+        return mem[row][col]
